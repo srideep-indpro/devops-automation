e501890a1a7287f408ee1e73aa27284aa1cff652
Srideep Bhattacharjee <srideepbhattacharjee@Srideeps-MacBook-Air.local>
2023-04-21 21:38:14 +0530

    first changes

---
+++ src/main/java/com/ilovejava/springbootdevops/GreetingController.java
@@ -0,0 +1,13 @@
+package com.ilovejava.springbootdevops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("hello")
+    public String greet(){
+        return "Hello! World";
+    }
+}
---
+++ src/main/java/com/ilovejava/springbootdevops/SpringBootDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.ilovejava.springbootdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevopsApplication.class, args);
+	}
+
+}
